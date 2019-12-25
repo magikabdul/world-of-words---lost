92b9ca340f090b8cd98c4ca998e86a9a70841600
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2019-12-25 18:21:33 +0100

    chore: app inital state

---
+++ front-end/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import WowApp from "./layouts/WowApp";
+
+ReactDOM.render(<WowApp />, document.getElementById("root"));
---
+++ front-end/src/layouts/WowApp.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+class WowApp extends Component {
+  state = {};
+
+  render() {
+    return (
+      <>
+        <CssBaseline />
+        <h1 style={{ textAlign: "center" }}>WowApp</h1>
+      </>
+    );
+  }
+}
+
+export default WowApp;
