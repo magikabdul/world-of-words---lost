f227394ef997cbbabf7cf84bd1526c49c1a72461
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2020-01-25 16:34:15 +0100

    feat: added routing for home, login, register and error

--- front-end/src/layouts/WowApp.js
+++ front-end/src/layouts/WowApp.js
@@ -1,17 +1,39 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Header from '../components/Header/Header';
+import ErrorPage from '../pages/Error';
 
 class WowApplication extends Component {
   state = {
     user: {
-      name: "",
+      name: '',
       isLogged: false
     }
   };
 
   render() {
-    return <BrowserRouter></BrowserRouter>;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' exact>
+            <Header
+              isLogged={this.state.user.isLogged}
+              name={this.state.user.name}
+            />
+          </Route>
+
+          <Route path='/login'></Route>
+
+          <Route path='/register'></Route>
+
+          <Route>
+            <ErrorPage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
