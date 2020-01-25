f6639a7bbd5460564bec0d9d25a30d8006e5756c
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2020-01-25 19:22:13 +0100

    feat: page not found

--- front-end/src/pages/Error.js
+++ front-end/src/pages/Error.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../scss/error.scss';
 
 const Error = () => {
   return (
-    <>
-      <h1>Error</h1>
-    </>
+    <div className='error--container'>
+      <h2 className='error--title'>404</h2>
+      <div className='error--message'>
+        <h3 className='error--message__title'>page not found</h3>
+        <p className='error--message__body'>
+          Why don't you try one of this pages instead?
+        </p>
+        <Link to='/'>
+          <button className='error--button'>Back home</button>
+        </Link>
+        <div className='error--footer'>
+          &copy; 2020 All rights reserved. Designed by{' '}
+          <strong className='error--footer__author'>magikabdul</strong>
+        </div>
+      </div>
+    </div>
   );
 };
 
