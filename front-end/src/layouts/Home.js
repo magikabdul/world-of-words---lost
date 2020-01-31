0ccae5e45de70eefef49aa43d1ac9ef40c44987b
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2020-01-31 23:37:10 +0100

    chore: chaged content rendering when user logged

--- front-end/src/layouts/Home.js
+++ front-end/src/layouts/Home.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from '../components/Header/Header';
 import Hero from '../components/Hero/Hero';
+import Menu from '../layouts/Menu/Menu';
+// const UserLogged = ({ name }) => {};
 
-const Home = ({ isLogged, name }) => {
+// const Default = () => {};
+
+const Home = ({ isLogged, isSuperUser, name }) => {
+  const [open, setOpen] = useState(true);
   return (
     <>
-      <Header isLogged={isLogged} name={name} />
-      <Hero />
+      {!isLogged && <Header isLogged={isLogged} name={name} />}
+      {!isLogged && <Hero />}
+      {isLogged && open && <Menu />}
     </>
   );
 };
