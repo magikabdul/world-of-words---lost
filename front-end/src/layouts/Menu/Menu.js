2aec04351530ee8e2b43d039f92817d1c9217d1d
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2020-01-31 23:38:29 +0100

    feat: inital state of side menu

---
+++ front-end/src/layouts/Menu/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './menu.scss';
+
+const Menu = () => {
+  return (
+    <div className='menu'>
+      <div className='menu--header'>
+        <div className='menu--header__logo'></div>
+        <div className='menu--header__title'>World of Words</div>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
