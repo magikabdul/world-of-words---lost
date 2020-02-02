1c46ed59227b6d7bfbb913b1932b521260df443f
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2020-02-02 19:41:26 +0100

    chore: added WorkingAreaHeader component

---
+++ front-end/src/components/WorkingArea/WorkingAreaHeader.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  color: #a9a9a9;
+  height: 3.25rem;
+  padding: 0.625rem 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Title = styled.span`
+  padding: 0.75rem 1.875rem;
+  font-size: 1.125rem;
+  font-weight: 300;
+  text-transform: capitalize;
+  color: #3c4858;
+`;
+
+const User = styled.span`
+  padding: 0.75rem 1.875rem;
+  align-self: center;
+`;
+
+const WorkingAreaHeader = ({ title, name }) => {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <User>{`Hello ${name}`}</User>
+    </Container>
+  );
+};
+
+export default WorkingAreaHeader;
