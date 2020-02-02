7c592458cf8e57f7e64a8bc463e55168b8cb9d04
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2020-02-02 19:20:24 +0100

    chore: added module for color definitions

---
+++ front-end/src/modules/Colors.js
@@ -0,0 +1,27 @@
+const orangeStart = '#ffa726';
+const orangeEnd = '#fb8c00';
+
+const greenStart = '#66bb6a';
+const greenEnd = '#43a047';
+
+const redStart = '#ef5350';
+const redEnd = '#e53935';
+
+const blueStart = '#26c6da';
+const blueEnd = '#00acc1';
+
+const violetStart = '#ab47bc';
+const violetEnd = '#8e24aa';
+
+export {
+  orangeStart,
+  orangeEnd,
+  greenStart,
+  greenEnd,
+  redStart,
+  redEnd,
+  blueStart,
+  blueEnd,
+  violetStart,
+  violetEnd
+};
