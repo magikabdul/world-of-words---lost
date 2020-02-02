2328d1b96cff280fc0207f8c94ec2d03e11b43c7
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2020-02-02 19:43:09 +0100

    feat: added WordsDatabase layout

---
+++ front-end/src/layouts/WorkingAreas/WordsDatabase/WordsDatabase.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import * as color from '../../../modules/Colors';
+import { IconCalendar, IconWords } from '../../../images/images-svg';
+
+import WorkingAreaContainer from '../../../components/WorkingArea/WorkingAreaContainer';
+import WorkingAreaHeader from '../../../components/WorkingArea/WorkingAreaHeader';
+import WorkingAreaBoard from '../../../components/WorkingArea/WorkingAreaBoard';
+import WorkingAreaRow from '../../../components/WorkingArea/WorkingAreaRow';
+import BoxWithIcon from '../../../components/WorkingArea/Boxes/BoxWithIcon';
+import { updateExpression } from '@babel/types';
+
+export default class WordsDatabase extends Component {
+  state = {
+    database: {
+      size: 0,
+      update: Date.now()
+    }
+  };
+
+  render() {
+    return (
+      <WorkingAreaContainer>
+        <WorkingAreaHeader title='words database' name={this.props.name} />
+        <WorkingAreaBoard>
+          <WorkingAreaRow>
+            <BoxWithIcon
+              icon={<IconWords />}
+              colorStart={color.violetStart}
+              colorEnd={color.violetEnd}
+              title='database size'
+              value={this.state.database.size}
+              units=' words'
+              iconMessage={<IconCalendar />}
+              message='updated today'
+            />
+            <BoxWithIcon />
+            <BoxWithIcon />
+          </WorkingAreaRow>
+        </WorkingAreaBoard>
+      </WorkingAreaContainer>
+    );
+  }
+}
