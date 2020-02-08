69fd9d3cfb2bbe8a6619d91986e34dc1dc537486
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2020-02-08 19:43:44 +0100

    chore: expanded color base and added new images

--- front-end/src/modules/Colors.js
+++ front-end/src/modules/Colors.js
@@ -13,8 +13,49 @@ const blueEnd = '#00acc1';
 const violetStart = '#ab47bc';
 const violetEnd = '#8e24aa';
 
-const WARNING = '#f44336';
+const WHITE = '#fff';
+
+const DEFAULT = '#999';
+const DEFAULT_BOX_SHADOW =
+  '0 2px 2px 0 rgba(153, 153, 153, 0.14), 0 3px 1px -2px rgba(153, 153, 153, 0.2), 0 1px 5px 0 rgba(153, 153, 153, 0.12)';
+const DEFAULT_HOVER_BOX_SHADOW =
+  '0 14px 26px -12px rgba(153, 153, 153, 0.42), 0 4px 23px 0px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(153, 153, 153, 0.2)';
+
+const PRIMARY = '#9c27b0';
+const PRIMARY_BOX_SHADOW =
+  '0 2px 2px 0 rgba(156, 39, 176, 0.14), 0 3px 1px -2px rgba(156, 39, 176, 0.2), 0 1px 5px 0 rgba(156, 39, 176, 0.12)';
+const PRIMARY_HOVER_BOX_SHADOW =
+  '0 14px 26px -12px rgba(156, 39, 176, 0.42), 0 4px 23px 0px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(156, 39, 176, 0.2)';
+
+const INFO = '#00acc1';
+const INFO_BOX_SHADOW =
+  '0 2px 2px 0 rgba(0, 172, 193, 0.14), 0 3px 1px -2px rgba(0, 172, 193, 0.2), 0 1px 5px 0 rgba(0, 172, 193, 0.12)';
+const INFO_HOVER_BOX_SHADOW =
+  '0 14px 26px -12px rgba(0, 172, 193, 0.42), 0 4px 23px 0px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 172, 193, 0.2)';
+
 const SUCCESS = '#4caf50';
+const SUCCESS_BOX_SHADOW =
+  '0 2px 2px 0 rgba(76, 175, 80, 0.14), 0 3px 1px -2px rgba(76, 175, 80, 0.2), 0 1px 5px 0 rgba(76, 175, 80, 0.12)';
+const SUCCESS_HOVER_BOX_SHADOW =
+  '0 14px 26px -12px rgba(76, 175, 80, 0.42), 0 4px 23px 0px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(76, 175, 80, 0.2)';
+
+const WARNING = '#ff9800';
+const WARNING_BOX_SHADOW =
+  '0 2px 2px 0 rgba(255, 152, 0, 0.14), 0 3px 1px -2px rgba(255, 152, 0, 0.2), 0 1px 5px 0 rgba(255, 152, 0, 0.12)';
+const WARNING_HOVER_BOX_SHADOW =
+  '0 14px 26px -12px rgba(255, 152, 0, 0.42), 0 4px 23px 0px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(255, 152, 0, 0.2)';
+
+const DANGER = '#f44336';
+const DANGER_BOX_SHADOW =
+  '0 2px 2px 0 rgba(244, 67, 54, 0.14), 0 3px 1px -2px rgba(244, 67, 54, 0.2), 0 1px 5px 0 rgba(244, 67, 54, 0.12)';
+const DANGER_HOVER_BOX_SHADOW =
+  '0 14px 26px -12px rgba(244, 67, 54, 0.42), 0 4px 23px 0px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(244, 67, 54, 0.2)';
+
+const ROSE = '#e91e63';
+const ROSE_BOX_SHADOW =
+  '0 2px 2px 0 rgba(233, 30, 99, 0.14), 0 3px 1px -2px rgba(233, 30, 99, 0.2), 0 1px 5px 0 rgba(233, 30, 99, 0.12)';
+const ROSE_HOVER_BOX_SHADOW =
+  '0 14px 26px -12px rgba(233, 30, 99, 0.42), 0 4px 23px 0px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(233, 30, 99, 0.2)';
 
 export {
   orangeStart,
@@ -27,6 +68,26 @@ export {
   blueEnd,
   violetStart,
   violetEnd,
+  WHITE,
+  DEFAULT,
+  DEFAULT_BOX_SHADOW,
+  DEFAULT_HOVER_BOX_SHADOW,
+  PRIMARY,
+  PRIMARY_BOX_SHADOW,
+  PRIMARY_HOVER_BOX_SHADOW,
+  INFO,
+  INFO_BOX_SHADOW,
+  INFO_HOVER_BOX_SHADOW,
+  SUCCESS,
+  SUCCESS_BOX_SHADOW,
+  SUCCESS_HOVER_BOX_SHADOW,
   WARNING,
-  SUCCESS
+  WARNING_BOX_SHADOW,
+  WARNING_HOVER_BOX_SHADOW,
+  DANGER,
+  DANGER_BOX_SHADOW,
+  DANGER_HOVER_BOX_SHADOW,
+  ROSE,
+  ROSE_BOX_SHADOW,
+  ROSE_HOVER_BOX_SHADOW
 };
