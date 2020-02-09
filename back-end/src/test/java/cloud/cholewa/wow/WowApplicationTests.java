0d0965b0a2c92d507d58ad21746c3806ed096443
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2020-02-09 12:08:08 +0100

    chore: backend initial commit

---
+++ back-end/src/test/java/cloud/cholewa/wow/WowApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.wow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
