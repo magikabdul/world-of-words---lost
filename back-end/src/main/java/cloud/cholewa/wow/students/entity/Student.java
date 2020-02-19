dd2fc14caee85179f01c4594cee91f9c7d8cf992
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2020-02-19 23:21:14 +0100

    chore: added entity student

---
+++ back-end/src/main/java/cloud/cholewa/wow/students/entity/Student.java
@@ -0,0 +1,68 @@
+package cloud.cholewa.wow.students.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
+
+@Entity
+@Table(name = "students")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Student implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
+    @NotNull
+    private String userName;
+    @NotNull
+    private String password;
+    @NotNull
+    private String mail;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastLogged;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
