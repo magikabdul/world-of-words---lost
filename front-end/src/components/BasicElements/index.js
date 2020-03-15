34249ca73a22319265f28c61275b76051f4b0e53
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2020-03-15 14:10:29 +0100

    chore: added export for Button, FormContainer, InputField and SimpleRow

---
+++ front-end/src/components/BasicElements/index.js
@@ -0,0 +1,6 @@
+import Button from './Button/Button';
+import FormContainer from './FormContainer/FormContainer';
+import InputField from './InputField/InputField';
+import SimpleRow from './SimpleRow/SimpleRow';
+
+export { Button, FormContainer, InputField, SimpleRow };
